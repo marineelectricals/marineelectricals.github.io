276ceb6db2a2813ffdcac17655c99cdcdefd766a
marineelectricals <104338034+marineelectricals@users.noreply.github.com>
2022-04-25 12:02:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Define spreadsheet URL.
+var prodss = 'https://docs.google.com/spreadsheets/d/11T168daP9NRW9aA8zEO7LpTcnDgeU6SKEs9JPq9XLWE/edit#gid=0';
+var qcss = 'https://docs.google.com/spreadsheets/d/11T168daP9NRW9aA8zEO7LpTcnDgeU6SKEs9JPq9XLWE/edit#gid=843385812';
+
+$('production').sheetrock({
+  url: prodss,
+  query: "select A",
+  fetchSize: 11
+});
+
 window.addEventListener('load', function() {
   console.log('Page is loaded');
 });
