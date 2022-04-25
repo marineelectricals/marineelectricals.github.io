934d46a0a9e1482e81dbcff782d0ba562d237599
marineelectricals <104338034+marineelectricals@users.noreply.github.com>
2022-04-25 17:22:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-// Define spreadsheet URL.
-var prodss = 'https://docs.google.com/spreadsheets/d/11T168daP9NRW9aA8zEO7LpTcnDgeU6SKEs9JPq9XLWE/edit#gid=0';
-var qcss = 'https://docs.google.com/spreadsheets/d/11T168daP9NRW9aA8zEO7LpTcnDgeU6SKEs9JPq9XLWE/edit#gid=843385812';
-
-$('production').sheetrock({
-  url: prodss,
-  query: "select A",
-  fetchSize: 11
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
 });
 
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      var options = {
+        body: 'Here is a notification body!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        },
+        actions: [
+          {action: 'explore', title: 'Explore this new world',
+            icon: 'images/checkmark.png'},
+          {action: 'close', title: 'Close notification',
+            icon: 'images/xmark.png'},
+        ]
+      };
+      reg.showNotification('Hello world!', options);
+    });
+  }
+}
 
 
 window.addEventListener('load', function() {
   console.log('Page is loaded');
 });
 
+/*
 const button = document.getElementById('subscribe');
 button.addEventListener('click', () => {
   Notification.requestPermission().then((result) => {
@@ -23,15 +40,33 @@ button.addEventListener('click', () => {
   });
 })
 
-Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
-});
-
-function displayNotification() {
-  if (Notification.permission === 'granted') {  
-    navigator.serviceWorker.getRegistration()
-    .then(function(reg){
-      reg.showNotification('Hello world!');
-    });
-  }
+function randomNotification() {
+  const randomItem = Math.floor(Math.random() * games.length);
+  const notifTitle = games[randomItem].name;
+  const notifBody = `Created by ${games[randomItem].author}.`;
+  const notifImg = `data/img/${games[randomItem].slug}.jpg`;
+  const options = {
+    body: notifBody,
+    icon: notifImg,
+  };
+  new Notification(notifTitle, options);
+  setTimeout(randomNotification, 30000);
 }
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+*/
