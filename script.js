9a6fba9f5bd49751d328f8c1b627427f8a824251
marineelectricals <104338034+marineelectricals@users.noreply.github.com>
2022-04-26 12:07:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ window.addEventListener('load', function() {
 const button = document.getElementById("subscribe");
 button.addEventListener("click", myFunction);
 
-function myFunction() {
-Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
+function displayNotification() {
+  if (Notification.permission == 'granted') {
 var title = "JavaScript Jeep";
 icon = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png';
 var body = "It's Your boarding time";
 var notification = new Notification(title, { body, icon });
+}
+
+function myFunction() {
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+    displayNotification();
 });
 }
