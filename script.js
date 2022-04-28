5775fadb2ed432714dbf7d0ad815af6ada8266ae
marineelectricals <104338034+marineelectricals@users.noreply.github.com>
2022-04-28 09:08:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let url = "https://sheets.googleapis.com/v4/spreadsheets/11T168daP9NRW9aA8zEO7LpTcnDgeU6SKEs9JPq9XLWE/values/PROD-QC-STAGES?key=AIzaSyBqf87IMEwSLnDl_ZSil2IDLS9oFszZgP8";
-
 window.addEventListener('load', function() {
   console.log('Page is loaded');
 });
@@ -12,7 +10,6 @@ Notification.requestPermission(function(status) {
     console.log('Notification permission status:', status);
 });
 }
-
 
 const button2 = document.getElementById("unsubscribe");
 button2.addEventListener("click", spawnNotification);
