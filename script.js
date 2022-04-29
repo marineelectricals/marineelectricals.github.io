7ba767cf61116aef1c8729448715358a08655533
marineelectricals <104338034+marineelectricals@users.noreply.github.com>
2022-04-29 08:58:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,42 @@ window.addEventListener('load', function() {
   console.log('Page is loaded');
 });
 
+const check = () => {
+  if (!('serviceWorker' in navigator)) {
+    throw new Error('No Service Worker support!')
+  }
+  if (!('PushManager' in window)) {
+    throw new Error('No Push API Support!')
+  }
+}
+const registerServiceWorker = async () => {
+  const swRegistration = await navigator.serviceWorker.register('service.js')
+  return swRegistration
+}
+const requestNotificationPermission = async () => {
+  const permission = await window.Notification.requestPermission()
+  // value of permission can be 'granted', 'default', 'denied'
+  // granted: user has accepted the request
+  // default: user has dismissed the notification permission popup by clicking on x
+  // denied: user has denied the request.
+  if (permission !== 'granted') {
+    throw new Error('Permission not granted for Notification')
+  }
+}
+const main = async () => {
+  check()
+  const swRegistration = await registerServiceWorker()
+  const permission = await requestNotificationPermission()
+}
+// main(); we will not call main in the beginning.
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
 const button = document.getElementById("subscribe");
-button.addEventListener("click", myFunction);
+button.addEventListener("click", main);
 
 function myFunction() {
 Notification.requestPermission(function(status) {
     console.log('Notification permission status:', status);
 });
-}
-
-const button2 = document.getElementById("unsubscribe");
-button2.addEventListener("click", spawnNotification);
-
-function spawnNotification(title, body, icon) {
-  var dept = "Production:", stage = "S1";
-  var title = dept + " Stage - " + stage;
-  var jobNumber = 15000.01;
-  var engineer = "engineer"
-  var bodyText = jobNumber + " (" + engineer + ")";
-  var img = "images/MEIL Logo.png";
-  var options = {
-    body: bodyText
-  }
-  var notification = new Notification(title, options);
 }
